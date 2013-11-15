5f2a278b9bc53893c662fa87cabee75655c74c0b
Inmakia <inmakia@gmail.com>
2013-11-15 14:27:11 +0100

    Register form edited and first test view done

--- subastasIS2_django/subastas/urls.py
+++ subastasIS2_django/subastas/urls.py
@@ -4,8 +4,6 @@ from subastas import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-
-    url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
-    url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
-    url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
+    
+    url(r'^test/$', views.test, name='test'),
 )
