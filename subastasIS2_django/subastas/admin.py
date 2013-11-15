a04afd6c8550e7b261b57f23472984bde25e3609
garinoth <cdavidrp@gmail.com>
2013-11-15 14:30:01 +0100

    Add first version for models

--- subastasIS2_django/subastas/admin.py
+++ subastasIS2_django/subastas/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from subastas.models import Auction, Bid, ExternalUser, InternalUser, Item
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+# from my_user_profile_app.models import User
+
+admin.site.register(Auction)
+admin.site.register(ExternalUser)
+admin.site.register(InternalUser)
+admin.site.register(Item)
