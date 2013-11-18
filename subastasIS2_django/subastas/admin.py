8a0acb97f2baccc42aaf2361476791d2a7bcf443
Garinoth <cdavidrp@gmail.com>
2013-11-18 11:42:59 +0100

    Register models and fix urls tests

--- subastasIS2_django/subastas/admin.py
+++ subastasIS2_django/subastas/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from subastas.models import Auction, Bid, ExternalUser, InternalUser, Item
+from subastas.models import ExternalUser, Item, Auction, Offer, Bid
 
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
 # from my_user_profile_app.models import User
 
-admin.site.register(Auction)
 admin.site.register(ExternalUser)
-admin.site.register(InternalUser)
 admin.site.register(Item)
+admin.site.register(Auction)
+admin.site.register(Offer)
+admin.site.register(Bid)
