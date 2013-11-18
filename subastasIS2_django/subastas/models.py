99697dfdf4e5bd0e217e2a0288800f48ed5c50d7
Garinoth <cdavidrp@gmail.com>
2013-11-18 20:53:42 +0100

    Merge develop

--- subastasIS2_django/subastas/models.py
+++ subastasIS2_django/subastas/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class ExternalUser(models.Model):
+class AucitonUser(models.Model):
     user = models.OneToOneField(User, primary_key=True)
     name = models.CharField(max_length=40)
     surname = models.CharField(max_length=40)
--- subastasIS2_django/subastas/views.py
+++ subastasIS2_django/subastas/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def index(request):
@@ -8,3 +10,13 @@ def index(request):
 def test(request):
     context = {}
     return render(request, 'subastas/index.html', context)
+
+
+def create_user(request):
+    #TODO
+    user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+    user.last_name = 'Lennon'
+    user.save()
+
+def login(request):
+    #TODO
