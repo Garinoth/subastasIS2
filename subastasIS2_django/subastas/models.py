e928a3d1b546ebcd2f20958daaf27cc12c7fa557
Garinoth <cdavidrp@gmail.com>
2013-11-23 23:07:41 +0100

    Change authentication to be done using email instead of username

--- subastasIS2_django/subastas/models.py
+++ subastasIS2_django/subastas/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 User._meta.get_field('email')._unique = True
+User.USERNAME_FIELD = 'email'
+User.REQUIRED_FIELDS = []
 
 
 class AuctionUser(models.Model):
