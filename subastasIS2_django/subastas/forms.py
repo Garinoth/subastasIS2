fb20ce7f6abe3130447195215fa93c5c973cbe4a
Garinoth <cdavidrp@gmail.com>
2013-11-26 12:50:28 +0100

    Add spanish translations

--- subastasIS2_django/subastas/forms.py
+++ subastasIS2_django/subastas/forms.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 from datetime import date
 import re
 from django.core.exceptions import ValidationError
@@ -8,23 +9,59 @@ from django.forms.widgets import Textarea
 from subastas.models import User, AuctionUser, Item, Auction, Offer, Bid
 
 
+default_error_messages = {
+    'required': 'Este campo es obligatorio',
+    'invalid': 'Este campo no es válido'
+}
+
+
 class UserForm(ModelForm):
 
+    username = CharField(
+        label='Nombre de usuario',
+        error_messages=default_error_messages,
+        max_length=30,
+    )
     password = CharField(
+        label='Contraseña',
+        error_messages=default_error_messages,
+        help_text='La contraseña debe tener una longitud de al menos 6 caracteres y contener letras, números y alguno de los siguientes caracateres: -+_!@#$%^&*.,?=',
         widget=PasswordInput,
     )
     confirm_password = CharField(
+        label='Confirme su contraseña',
+        error_messages=default_error_messages,
+        help_text='La contraseña debe tener una longitud de al menos 6 caracteres y contener letras, números y alguno de los siguientes caracateres: -+_!@#$%^&*.,?=',
         widget=PasswordInput,
     )
-    confirm_email = EmailField()
+    email = EmailField(
+        label='Correo electrónico',
+        error_messages=default_error_messages,
+    )
+    confirm_email = EmailField(
+        label='Confirme su correo electrónico',
+        error_messages=default_error_messages,
+    )
+    first_name = CharField(
+        label='Nombre',
+        error_messages=default_error_messages,
+        max_length=30,
+    )
+    last_name = CharField(
+        label='Apellidos',
+        error_messages=default_error_messages,
+        max_length=30,
+    )
 
     class Meta:
         model = User
         fields = ['username',
+                  'email',
+                  'confirm_email',
                   'password',
+                  'confirm_password',
                   'first_name',
-                  'last_name',
-                  'email']
+                  'last_name']
 
     def __init__(self, *args, **kwargs):
 
@@ -42,21 +79,23 @@ class UserForm(ModelForm):
             self.cleaned_data.get('confirm_email')):
 
             raise ValidationError(
-                "Email addresses must match."
+                "Las direcciones de correo deben coincidir",
+                code='confirm',
             )
 
         if (self.cleaned_data.get('password') !=
             self.cleaned_data.get('confirm_password')):
 
             raise ValidationError(
-                "Passwords must match."
+                "Las contraseñas deben coincidir",
+                code='confirm',
             )
 
         password = self.cleaned_data.get('password')
         regex = r'^(?=.*[a-zA-Z])(?=.*\d)(?=.*[-+_!@#$%^&*.,?=]).{6,}$'
         if not re.match(regex, password):
-          raise ValidationError(
-                "Passwords must be at least 6 characters long and contain at least a number and one of these characters: -+_!@#$%^&*.,?=."
+            raise ValidationError(
+                code='invalid',
             )
 
         return self.cleaned_data
@@ -64,16 +103,36 @@ class UserForm(ModelForm):
 
 class AuctionUserForm(ModelForm):
 
-    birth_date = DateField(
-        widget=SelectDateWidget(years=range(date.today().year, 1920, -1)),
-    )
     address = CharField(
+        label='Dirección',
         max_length=100,
         widget=Textarea,
         required=False,
     )
+    postal_code = CharField(
+        label='Código Postal',
+        max_length=5,
+    )
+    city = CharField(
+        label='Ciudad',
+        max_length=40,
+    )
+    country = CharField(
+        label='País',
+        max_length=40,
+    )
+    phone_number = CharField(
+        label='Número de teléfono',
+        max_length=20,
+    )
+    birth_date = DateField(
+        label='Fecha de Nacimiento',
+        error_messages=default_error_messages,
+        widget=SelectDateWidget(years=range(date.today().year, 1920, -1)),
+    )
     tos = BooleanField(
-        error_messages={'required': 'You must accept the terms and conditions'},
+        error_messages={
+            'required': 'Debe aceptar los Términos y Condiciones'},
     )
 
     class Meta:
@@ -121,9 +180,13 @@ class BidForm(ModelForm):
                   'auction',
                   'points']
 
+
 class ActivationForm(Form):
-    email = EmailField()
+    email = EmailField(
+        label='Correo electrónico',
+    )
     password = CharField(
+        label='Contraseña',
         widget=PasswordInput,
     )
     activation_key = CharField(max_length=40)
@@ -135,12 +198,12 @@ class ActivationForm(Form):
         user = authenticate(email=email, password=password)
         if not user:
             raise ValidationError(
-                "Invalid login"
+                "El usuario y la contraseña no son válidos"
             )
         auction_user = AuctionUser.objects.get(user=user)
         if auction_user.activation_key != activation_key:
             raise ValidationError(
-                "The activation key is not valid",
+                "La clave de activación no es válida",
             )
 
         return self.cleaned_data
