51600c110f43da33984ae63bceb7aa588caf7671
Garinoth <cdavidrp@gmail.com>
2013-12-01 22:35:26 +0100

    Add redirect from hom

--- subastasIS2_django/subastasIS2_django/urls.py
+++ subastasIS2_django/subastasIS2_django/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # url(r'^$', 'subastasIS2_django.views.home', name='home'),
+    url(r'^$', RedirectView.as_view(url='/subastas/')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^subastas/', include('subastas.urls')),
-
 )
